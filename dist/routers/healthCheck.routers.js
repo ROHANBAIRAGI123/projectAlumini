6c72ec78d76f063659d90efd8d96e1825309227e
Rohan Bairagi <143432752+ROHANBAIRAGI123@users.noreply.github.com>
2025-03-30 17:35:51 +0530

    Stop tracking dist directory

--- dist/routers/healthCheck.routers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const healthCheck_controllers_1 = require("../controllers/healthCheck.controllers");
-const router = (0, express_1.Router)();
-router.route("/").get(healthCheck_controllers_1.healthCheckController);
-exports.default = router;
